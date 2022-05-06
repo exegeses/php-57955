0f7c94abac4a5c52bf73c2e91da2d7e394c19157
Marcos <exegeses@gmail.com>
2022-05-06 16:01:59 -0300

    Aplicación de modificación de una marca + confirmación de baja de un amarca

--- catalogo/funciones/marcas.php
+++ catalogo/funciones/marcas.php
@@ -17,6 +17,23 @@
 
     }
 
+    function verMarcaPorID()
+    {
+        $idMarca = $_GET['idMarca'];
+        $link = conectar();
+        $sql='SELECT idMarca, mkNombre
+                FROM marcas 
+                WHERE idMarca = '. $idMarca;
+        try {
+            $resultado = mysqli_query( $link, $sql );
+            $marca = mysqli_fetch_assoc($resultado);
+        }catch ( Exception $e ){
+            echo $e->getMessage();
+            return false;
+        }
+        return $marca;
+    }
+
     function agregarMarca( ) : bool
     {
         $mkNombre = $_POST['mkNombre'];
@@ -34,9 +51,49 @@
             echo $e->getMessage();
             return false;
         }
-
     }
 
+    function modificarMarca()
+    {
+        $idMarca = $_POST['idMarca'];
+        $mkNombre = $_POST['mkNombre'];
+        $link = conectar();
+        $sql="UPDATE marcas
+                 SET mkNombre = '". $mkNombre ."' 
+                WHERE idmarca = ".  $idMarca;
+        try {
+            $resultado = mysqli_query($link,$sql);
+            return $resultado ;
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    /**
+     * función para verificar si hay productos
+     * asignados a una marca
+     * @return int
+     */
+    function verificarProdPorMarca()
+    {
+        $idMarca = $_GET['idMarca'];
+        $link = conectar();
+        $sql = "SELECT 1 FROM productos
+                    WHERE idMarca = ".$idMarca;
+        try {
+            $resultado = mysqli_query($link,$sql);
+            $cantidad = mysqli_num_rows($resultado);
+            return $cantidad;
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+            return 0;
+        }
+    }
 
 /*
  * listarMarcas()
